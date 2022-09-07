4c2b6a5a0863ea918d304aadb77f4e44ca398600
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-09-07 17:39:57 -0500

    Fix linters errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable arrow-body-style */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/prop-types */
 import React from 'react';
 import '../styles/Calculator.css';
 import calculate from '../logic/calculate';
@@ -14,11 +10,10 @@ export default class Calculator extends React.Component {
       next: null,
       operation: null,
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.HandleClick = this.HandleClick.bind(this);
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  handleClick(event) {
+  HandleClick(event) {
     const buttonUse = event.target.textContent;
     const result = calculate(this.state, buttonUse);
     this.setState(result);
@@ -34,34 +29,36 @@ export default class Calculator extends React.Component {
           {operation}
           {next}
         </p>
-        <Buttons handler={this.handleClick} />
+        <Buttons handler={this.HandleClick} />
       </div>
     );
   }
 }
 
+/* eslint-disable react/prop-types */
 const Buttons = (props) => {
+  const { handler } = props;
   return (
     <div className="Container-buttons">
-      <button onClick={props.handler} type="button" className="btn0">0</button>
-      <button onClick={props.handler} type="button" className="btn2">2</button>
-      <button onClick={props.handler} type="button" className="btn1">1</button>
-      <button onClick={props.handler} type="button" className="btn3">3</button>
-      <button onClick={props.handler} type="button" className="btn4">4</button>
-      <button onClick={props.handler} type="button" className="btn5">5</button>
-      <button onClick={props.handler} type="button" className="btn6">6</button>
-      <button onClick={props.handler} type="button" className="btn7">7</button>
-      <button onClick={props.handler} type="button" className="btn8">8</button>
-      <button onClick={props.handler} type="button" className="btn9">9</button>
-      <button onClick={props.handler} type="button" className="btn-add orange">+</button>
-      <button onClick={props.handler} type="button" className="btn-less orange">-</button>
-      <button onClick={props.handler} type="button" className="btn-division orange">÷</button>
-      <button onClick={props.handler} type="button" className="btn-multiplication orange">*</button>
-      <button onClick={props.handler} type="button" className="btn-percentage">%</button>
-      <button onClick={props.handler} type="button" className="btn-moremin">+/-</button>
-      <button onClick={props.handler} type="button" className="btn.dot">.</button>
-      <button onClick={props.handler} type="button" className="btn-equal orange">=</button>
-      <button onClick={props.handler} type="button" className="btn-Ac">AC</button>
+      <button onClick={handler} type="button" className="btn0">0</button>
+      <button onClick={handler} type="button" className="btn2">2</button>
+      <button onClick={handler} type="button" className="btn1">1</button>
+      <button onClick={handler} type="button" className="btn3">3</button>
+      <button onClick={handler} type="button" className="btn4">4</button>
+      <button onClick={handler} type="button" className="btn5">5</button>
+      <button onClick={handler} type="button" className="btn6">6</button>
+      <button onClick={handler} type="button" className="btn7">7</button>
+      <button onClick={handler} type="button" className="btn8">8</button>
+      <button onClick={handler} type="button" className="btn9">9</button>
+      <button onClick={handler} type="button" className="btn-add orange">+</button>
+      <button onClick={handler} type="button" className="btn-less orange">-</button>
+      <button onClick={handler} type="button" className="btn-division orange">÷</button>
+      <button onClick={handler} type="button" className="btn-multiplication orange">*</button>
+      <button onClick={handler} type="button" className="btn-percentage">%</button>
+      <button onClick={handler} type="button" className="btn-moremin">+/-</button>
+      <button onClick={handler} type="button" className="btn.dot">.</button>
+      <button onClick={handler} type="button" className="btn-equal orange">=</button>
+      <button onClick={handler} type="button" className="btn-Ac">AC</button>
     </div>
   );
 };
