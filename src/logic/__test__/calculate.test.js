1e062291d4f0c80df8a6dc59f1dfceac68506943
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-09-14 15:38:18 -0500

    Add unit test for the Calculate & operate file

---
+++ src/logic/__test__/calculate.test.js
@@ -0,0 +1,29 @@
+import calculate from '../calculate';
+
+describe('Calculate File', () => {
+  it('Testing AC', () => {
+    const obj = '';
+    const buttonName = 'AC';
+
+    expect(calculate(obj, buttonName)).toEqual({
+      total: '0',
+      next: null,
+      operation: null,
+    });
+  });
+
+  it('Testing obj.net & buttonName are 0', () => {
+    const obj = { total: '', next: '0', operation: '' };
+    const buttonName = '0';
+    expect(calculate(obj, buttonName)).toEqual({});
+  });
+
+  it('Any "if" is true', () => {
+    const obj = '0';
+    const buttonName = '123';
+    expect(calculate(obj, buttonName)).toEqual({
+      next: buttonName,
+      total: null,
+    });
+  });
+});
---
+++ src/logic/__test__/operate.test.js
@@ -0,0 +1,23 @@
+import operate from '../operate';
+
+describe('Operate File', () => {
+  it('Operation +', () => {
+    expect(operate(1, 2, '+')).toBe('3');
+  });
+
+  it('Operation -', () => {
+    expect(operate(1, 2, '-')).toBe('-1');
+  });
+
+  it('Operation *', () => {
+    expect(operate(1, 2, '*')).toBe('2');
+  });
+
+  it('Operation ÷', () => {
+    expect(operate(1, 2, '÷')).toBe('0.5');
+  });
+
+  it('Operation %', () => {
+    expect(operate(7, 3, '%')).toBe('1');
+  });
+});
